2e121a2a4834a9bfa63637f7262b902684bd920f
soufianesaliki <soufianesaliki1@gmail.com>
2024-01-21 06:39:26 -0800

    fixing monty.h

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include "monty.h"
-
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
 * main - monty code interpreter
 * @argc: number of arguments
@@ -11,7 +11,6 @@ int main(int argc, char *argv[])
 	char *content;
 	FILE *file;
 	size_t size = 0;
-	bus_t bus = {NULL, NULL, NULL, 0};
 	ssize_t read_line = 1;
 	stack_t *stack = NULL;
 	unsigned int counter = 0;
